2a11447f8ea5095f2664c3841d16e5dd7e159e7b
Gabriel Varela <gabrielvarelaneto11@gmail.com>
2022-07-23 16:20:36 -0300

    Adding readme

--- src/__tests__/components/TasksList.spec.tsx
+++ src/__tests__/components/TasksList.spec.tsx
@@ -11,6 +11,7 @@ let tasks: {
 
 let mockedRemoveTask: jest.Mock;
 let mockedToggleTaskDone: jest.Mock;
+let mockedEditTask: jest.Mock;
 
 describe('MyTasksList', () => {
 
@@ -38,7 +39,7 @@ describe('MyTasksList', () => {
   });
 
   it('should be able to render all tasks', () => {
-    const { getByText } = render(<TasksList tasks={tasks} removeTask={mockedRemoveTask} toggleTaskDone={mockedToggleTaskDone} />)
+    const { getByText } = render(<TasksList tasks={tasks} removeTask={mockedRemoveTask} toggleTaskDone={mockedToggleTaskDone} editTask={mockedEditTask} />)
     
     getByText('Primeiro todo');
     getByText('Segundo todo');
@@ -46,7 +47,7 @@ describe('MyTasksList', () => {
   });
 
   it('should be able to handle "removeTask" event', () => {
-    const { getByTestId } = render(<TasksList tasks={tasks} removeTask={mockedRemoveTask} toggleTaskDone={mockedToggleTaskDone} />)
+    const { getByTestId } = render(<TasksList tasks={tasks} removeTask={mockedRemoveTask} toggleTaskDone={mockedToggleTaskDone}  editTask={mockedEditTask}  />)
     const firstTaskTrashIcon = getByTestId('trash-0');
 
     fireEvent(firstTaskTrashIcon, 'press');
@@ -55,7 +56,7 @@ describe('MyTasksList', () => {
   });
 
   it('should be able to handle "toggleTaskDone" event', () => {    
-    const { getByText } = render(<TasksList tasks={tasks} removeTask={mockedRemoveTask} toggleTaskDone={mockedToggleTaskDone} />)
+    const { getByText } = render(<TasksList tasks={tasks} removeTask={mockedRemoveTask} toggleTaskDone={mockedToggleTaskDone}  editTask={mockedEditTask}  />)
     const secondTask = getByText('Segundo todo');
 
     fireEvent.press(secondTask);
